2e1161e967c27522ef0dffccf5adfb1afca78f19
yift <me@ykaplan.me>
2025-09-27 14:00:45 +0100

    Remove add

--- src/parser/span.rs
+++ src/parser/span.rs
@@ -1,4 +1,4 @@
-use std::{cmp::max, cmp::min, fmt::Display, ops::Add};
+use std::fmt::Display;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Span {
@@ -9,15 +9,5 @@ pub struct Span {
 impl Display for Span {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "At {} - {}", self.start, self.end)
-    }
-}
-
-impl Add<&Span> for Span {
-    type Output = Span;
-    fn add(self, rhs: &Span) -> Self::Output {
-        Span {
-            start: min(self.start, rhs.start),
-            end: max(self.end, rhs.end),
-        }
     }
 }
